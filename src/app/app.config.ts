17964f892a4c37360cfa01725854110004de9ee0
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2025-02-18 12:08:55 +0000

    Completes homepage design

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,17 +9,13 @@ import {
 } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { NOTYF, notyfFactory } from './shared/utils/notyf.token';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
-    { provide: NOTYF, useFactory: notyfFactory },
-    provideClientHydration(withEventReplay()),
+    { provide: NOTYF, useFactory: notyfFactory }, provideClientHydration(withEventReplay()),
   ],
 };
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
+import { ProductCardComponent } from '../shared/components/product-card/product-card.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProductCardComponent, FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.sass',
 })
